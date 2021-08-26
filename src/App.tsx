899f146bf9d591202edb212c4bc41b17e24af72d
sig03 <sig03@jiran.com>
2021-08-26 02:00:08 +0000

    fix typescript - fix typescript any type

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { MouseEvent } from 'react';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import Divider from '@material-ui/core/Divider';
@@ -6,11 +7,13 @@ import './style.css';
 function App() {
   const list = ['aaaaaa', 'bbbbbb', 'cccccc', 'dddddd', 'eeeeee', 'ffffff', 'gggggg', 'hhhhhh', 'iiiiii', 'jjjjjj', 'kkkkkk'];
 
-  const handleMouseOver = (e:any) => {    
-    e.target.classList.add('Mui-focusVisible');  
+  const handleMouseOver = (e: MouseEvent) => {
+    const target: HTMLElement = e.target as HTMLElement;
+    target.classList.add('Mui-focusVisible');
   }
-  const handleMouseOut = (e:any) => {
-    e.target.classList.remove('Mui-focusVisible');    
+  const handleMouseOut = (e: MouseEvent) => {
+    const target: HTMLElement = e.target as HTMLElement;
+    target.classList.remove('Mui-focusVisible');    
   }
 
   return (
